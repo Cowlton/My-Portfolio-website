a92110720503d03ae9a8335e87c9c63c1fbba106
Colton Briggs <wolflink013@gmail.com>
2025-11-24 08:12:22 -0800

    file Name fix

---
+++ JavaScript.js
@@ -0,0 +1,4 @@
+﻿let Poem = "People make bad choices\nWhen their\nMad\nOr scared\nOR stressed.\nBut throw a little love their way.";
+console.log(Poem);
+let sentence = "You said your friends where \"Love Experts\".";
+console.log(sentence);
